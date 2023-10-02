2ff88eeda8ea1b060f0fdf59bf10c2a897f7f17c
Veekthor01 <veekthor01@gmail.com>
2023-10-02 15:05:56 +0300

    import db file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
