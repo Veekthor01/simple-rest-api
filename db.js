519093ccf214187699675c0f20f4f78434aac54b
Veekthor01 <veekthor01@gmail.com>
2023-10-04 16:41:21 +0300

    add close db function

--- db.js
+++ db.js
@@ -12,8 +12,21 @@ async function connectToMongoDB() {
     return client.db(); // Return the database instance
   } catch (err) {
     console.error(`MongoDB connection error: ${err}`);
-    throw err; // Propagate the error
+    throw err;
   } 
 }
 
-module.exports = connectToMongoDB;
+// Add a function to close the MongoDB connection
+async function closeMongoDBConnection() {
+    try {
+      await client.close();
+      console.log("MongoDB connection closed");
+    } catch (err) {
+      console.error(`Error closing MongoDB connection: ${err}`);
+    }
+  }
+  
+  module.exports = {
+    connectToMongoDB,
+    closeMongoDBConnection,
+  };
