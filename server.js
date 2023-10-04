cbf2ab58ea637ff84f348b8b8d4558e4be09b168
Veekthor01 <veekthor01@gmail.com>
2023-10-04 16:43:21 +0300

    add sigterm signal

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const apiRouter = require("./apiRoutes");
-const connectToMongoDB = require("./db"); // Import the MongoDB connection function
+const { connectToMongoDB } = require("./db");
+const { closeMongoDBConnection } = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectToMongoDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,4 +16,14 @@ app.use('/api/crud', apiRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.');
+    console.log('Closing http server.');
+    server.close(() => {
+      console.log('Http server closed.');
+      closeMongoDBConnection();
+      process.exit(0);
+    });
+  });
 
